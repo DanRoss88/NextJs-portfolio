467d9bdeeb7a1c8f315d3537eb2ea777cf25f70f
DanRoss88 <danielmartinross@gmail.com>
2023-06-20 07:20:12 -0700

    nextjs tutorial

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,9 @@
-import Head from 'next/head';
-import Layout from '../../components/layout';
+import Head from "next/head";
+import Layout from "../../components/layout";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 
-
-import { getAllPostIds, getPostData } from '../../lib/posts';
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -22,18 +23,19 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export default function Post({postData}) {
+export default function Post({ postData }) {
   return (
-  <Layout>
-    <Head>
-    <title>{postData.title}</title>
-    </Head>
-    <br />
-    {postData.id}
-    <br />
-    {postData.date}
-    <br />
-    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-  </Layout>
+<Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
   );
 }
