d4cc937805924116638c4992eaa8042be15ed807
DanRoss88 <danielmartinross@gmail.com>
2023-06-21 11:21:40 -0700

    navlink adjustment for mobile

--- components/navbar.js
+++ components/navbar.js
@@ -5,16 +5,16 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <Link href="/" passHref>
-        <span className={utilStyles.navLink}>Home</span>
+      <span className={`${utilStyles.navLink} navLink`}>Home</span>
       </Link>
       <Link href="/projects" passHref>
-        <span className={utilStyles.navLink}>Projects</span>
+        <span className={`${utilStyles.navLink} navLink`}>Projects</span>
       </Link>
       <Link href="/resume" passHref>
-        <span className={utilStyles.navLink}>Resume</span>
+      <span className={`${utilStyles.navLink} navLink`}>Resume</span>
       </Link>
       <Link href="/contact" passHref>
-        <span className={utilStyles.navLink}>Contact</span>
+      <span className={`${utilStyles.navLink} navLink`}>Contact</span>
       </Link>
     </nav>
   );
